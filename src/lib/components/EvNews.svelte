f91bf674a3086f15e81efab75491b3d26c14d0b8
Todor Garnev <tgarnev@abv.bg>
2023-11-27 19:00:08 +0200

    updated ev new component styles

--- src/lib/components/EvNews.svelte
+++ src/lib/components/EvNews.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
+	import BigEvNewsImg1 from "../assets/big-ev-news-1.png";
+	import BigEvNewsImg2 from "../assets/big-ev-news-2.png";
+	import BigEvNewsImg3 from "../assets/big-ev-news-3.png";
+	import SmallEvNewsImg1 from "../assets/small-ev-news-1.png";
+	import SmallEvNewsImg2 from "../assets/small-ev-news-2.png";
+	import SmallEvNewsImg3 from "../assets/small-ev-news-3.png";
+	import SmallEvNewsImg4 from "../assets/small-ev-news-4.png";
+
 	const primaryNews = [
 		{
 			date: "04.09.2023",
-			description: "Tesla API, Пожар на  Лутън, Mercedes eActros 600"
+			description: "Tesla API, Пожар на Лутън, Mercedes eActros 600"
 		},
 		{
 			date: "04.09.2023",
@@ -16,7 +24,7 @@
 	const secondaryNews = [
 		{
 			date: "04.09.2023",
-			description: "Tesla M3 Highland, Cybertruck, CATL"
+			description: "TESLA M3 Highland, Cybertruck, CATL"
 		},
 		{
 			date: "04.09.2023",
@@ -31,6 +39,34 @@
 			description: "Tesla новини, NACS + VW, Model S Plaid и сцеплението"
 		}
 	];
+
+	const getBigImg = (i: number) => {
+		switch (i) {
+			case 1:
+				return BigEvNewsImg1;
+			case 2:
+				return BigEvNewsImg2;
+			case 3:
+				return BigEvNewsImg3;
+			default:
+				break;
+		}
+	};
+
+	const getSmallImg = (i: number) => {
+		switch (i) {
+			case 1:
+				return SmallEvNewsImg1;
+			case 2:
+				return SmallEvNewsImg2;
+			case 3:
+				return SmallEvNewsImg3;
+			case 4:
+				return SmallEvNewsImg4;
+			default:
+				break;
+		}
+	};
 </script>
 
 <section class="wrapper">
@@ -41,10 +77,12 @@
 	</header>
 
 	<div class="primary-items">
-		{#each primaryNews as item}
+		{#each primaryNews as item, i}
 			<div class="news-card big-news-card">
 				<div class="card-image">
 					<div class="overlay" />
+
+					<img src={getBigImg(i + 1)} alt="Big News" />
 
 					<div class="title">{item.date}</div>
 				</div>
@@ -55,10 +93,12 @@
 	</div>
 
 	<div class="secondary-items">
-		{#each secondaryNews as item}
+		{#each secondaryNews as item, i}
 			<div class="news-card small-news-card">
 				<div class="card-image">
 					<div class="overlay" />
+
+					<img src={getSmallImg(i + 1)} alt="Small News" />
 
 					<div class="title">{item.date}</div>
 				</div>
@@ -71,20 +111,20 @@
 
 <style>
 	section {
-		padding-block: 4rem;
+		padding-block: 4rem 4.4rem;
 		background: var(--stripes-bg-2);
 	}
 
 	.primary-items {
-		margin-bottom: 3rem;
+		margin-bottom: 3.2rem;
 		display: grid;
 		grid-template-columns: repeat(3, 1fr);
-		gap: 3rem;
+		gap: 3.2rem;
 	}
 
 	.secondary-items {
 		display: grid;
 		grid-template-columns: repeat(4, 1fr);
-		gap: 3rem;
+		gap: 3.2rem;
 	}
 </style>
